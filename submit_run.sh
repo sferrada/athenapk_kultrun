2f844c1b4814f6834aa13732c88b865d48333223
Simon <simonfch72@gmail.com>
2023-10-17 08:46:32 -0300

    Backup

--- submit_run.sh
+++ submit_run.sh
@@ -9,4 +9,4 @@
 module load openmpi/4.1.5 gcc/12.2.0 hdf5/1.14.1-2_openmpi-4.1.5_parallel cuda/12.2
 
 cd /home/sferrada/athenapk_kultrun/
-mpirun ./athenapk/build-host/bin/athenaPK -i ./outputs/turb_nGPU1_nc256_M##_B0.05/turbulence_philipp_nc256.in > ./outputs/turb_nGPU1_nc256_M##_B0.05/turbulence_philipp_nc256.out
+mpirun ./athenapk/build-host/bin/athenaPK -i ./outputs/turb_nGPU1_nc64_M##_B0.05/turbulence_philipp_nc64.in > ./outputs/turbulence_philipp_nc64.out
