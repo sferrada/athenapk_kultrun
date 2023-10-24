d78ffcc5ecec83f5d305291a5cdd751a774d4921
Simon Ferrada Chamorro <simonfch72@gmail.com>
2023-10-24 20:37:05 +0200

    Backup

--- modify_input_file.py
+++ modify_input_file.py
@@ -1,7 +1,12 @@
+import os
 import sys
-from src.commons import (read_athenapk_config_file,
-                         write_athenapk_config_file,
-                         format_athenapk_config_file)
+from src.commons import (
+    load_config_file,
+    output_directory_name,
+    read_athenapk_config_file,
+    write_athenapk_config_file,
+    modify_athenapf_config_file
+)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -9,38 +14,49 @@ if __name__ == "__main__":
         sys.exit(1)
 
     input_file = sys.argv[1]
-    output_file = input_file.replace('.in', '_modified.in')
+    config_file = load_config_file('config.yaml')
+    output_directory = output_directory_name(config_file)
+
+    # Specify the directory path you want to create
+    directory_path = os.path.join("outputs", output_directory)
+
+    # Check if the directory exists, and if not, create it
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    else:
+        print(f"ERROR: directory '{directory_path}' already exists.")
+        sys.exit(1)
 
     # Read the configuration file
-    config = read_athenapk_config_file(input_file)
-    print(config['parthenon/mesh'])
+    config = read_athenapk_config_file(input_file, skip_header=False)
 
-    # # Format the config dictionary to be printed
-    # format_athenapk_config_file(config, print_formatted=True)
-
-    # Modify the configuration if needed
-    number_of_cells = 128
-    config['parthenon/mesh'][1]  = ('nx1', number_of_cells, 'Number of zones in X1-direction')
-    config['parthenon/mesh'][6]  = ('nx2', number_of_cells, 'Number of zones in X2-direction')
-    config['parthenon/mesh'][11] = ('nx3', number_of_cells, 'Number of zones in X3-direction')
-    config_mod = {
-        # Todo : If I do what's below, it replaces all the non-modified fields of the section
-        # Todo cont'd: thus, I need to do something about it, it should only replace the wanted
-        # Todo cont'd: fields and leave the rest untouched... for the moment I settled with the above
-        # 'parthenon/mesh': [
-        #     ('nx1', number_of_cells, 'Number of zones in X1-direction'),
-        #     ('nx2', number_of_cells, 'Number of zones in X2-direction'),
-        #     ('nx3', number_of_cells, 'Number of zones in X3-direction')
-        # ],
-        'parthenon/meshblock': [
-            ('nx1', number_of_cells // 4, None),
-            ('nx2', number_of_cells // 4, None),
-            ('nx3', number_of_cells // 2, None)
-        ]
+    # Custom column widths for specific sections
+    custom_column_widths = {
+        'global': (10, 10, 10),  # General column widths
+        'comment': (9, 10, 10),  # Custom widths for the '<comment>' section
+        'hydro': (1, 14, 14),  # Custom widths for the '<hydro>' section
+        'problem/turbulence': (12, 8, 10),  # Custom widths for the '<problem/turbulence>' section
+        'modes': (7, 2, 10)  # Custom widths for the '<modes>' section
     }
 
-    # Write the modified configuration back to the file
-    write_athenapk_config_file(output_file, config | config_mod)
+    # List of modifications to apply
+    modifications = [
+        ('hydro', 'eos', config_file["equation_of_state"]),  # Modify the equation of stat in <hydro>
+        ('problem/turbulence', 'b0', config_file["initial_magnetic_field"]),
+        ('problem/turbulence', 'accel_rms', config_file["acceleration_field_rms"])
+    ]
 
-    print(f"Modified configuration saved to {output_file}")
+    # Apply modifications to the configuration
+    modified_config = modify_athenapf_config_file(config, modifications)
+
+    # Write the configuration file with custom column widths
+    output_file = os.path.join("outputs", output_directory, "turbulence_philipp.in")
+    write_athenapk_config_file(
+        output_file,
+        config_dict=modified_config,
+        column_widths=custom_column_widths,
+        number_of_cells=int(config_file["number_of_cells"])
+    )
+
+    print(f"Modified configuration saved to {os.path.join(output_file)}")
 
--- src/commons.py
+++ src/commons.py
@@ -1,26 +1,105 @@
-def read_athenapk_config_file(filename: str) -> dict:
+import yaml
+from typing import (Union)
+
+def validate_parameter(param_to_validate,
+                       default):
+    """
+    Validate and substitute a parameter with a default value if it is set to None.
+    
+    Args:
+        param_to_validate: The input parameter to validate.
+        default: The default value to use if the parameter is set to None.
+    
+    Returns:
+        The `param_to_validate` if it is not None, otherwise, the `default` value.
+    """
+    return param_to_validate if param_to_validate is not None else default
+
+
+def output_directory_name(config_dict):
+    base_name = "Turb"
+
+    base_name += "_nGPUs" + str(config_dict["number_of_gpus"])
+    base_name += "_ncells" + str(config_dict["number_of_cells"])
+    base_name += "_accelrms" + str(config_dict["acceleration_field_rms"])
+    base_name += "_B" + str(config_dict["initial_magnetic_field"])
+    base_name += "_" + config_dict["equation_of_state"][:5].capitalize()
+    return base_name
+
+def load_config_file(config_file_path: str,
+                     override_config: Union[dict, None] = None) -> dict:
+    """
+    Load information from a YAML configuration file into a Python dictionary,
+    optionally allowing parameter overrides.
+
+    Args:
+        config_file_path (str): The path to the configuration file.
+        override_config (Union[dict, None], optional): Parameters to override in the loaded configuration.
+            Defaults to None.
+
+    Returns:
+        dict: A dictionary with the parsed configuration information.
+    """
+    _override_config = validate_parameter(override_config, default={})
+    with open(config_file_path) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+    for key in _override_config:
+        if key not in config.keys():
+            config[key] = {}
+        for subkey in _override_config[key]:
+            try:
+                config[key][subkey] = _override_config[key][subkey]
+            except KeyError:
+                config[key] = {subkey: _override_config[key][subkey]}
+
+    try:
+        if config['density_powerlaw_idx'] == 0:
+            config['central_density'] = config['density_at_reference']
+            config['density_at_reference'] = None
+    except KeyError:
+        pass
+
+    try:
+        if config['dust_temperature_powerlaw_idx'] == 0:
+            config['dust_temperature'] = config['dust_temperature_at_reference']
+            config['dust_temperature_at_reference'] = None
+    except KeyError:
+        pass
+    return config
+
+
+def read_athenapk_config_file(filename: str, 
+                              skip_header: bool = True) -> dict:
     """
     Read and parse an AthenaPK configuration file.
 
     Parameters:
         filename (str): The name of the configuration file to read.
+        skip_header (bool): Whether to skip the first section (e.g., <comment>). Default is True.
 
     Returns:
         dict: A dictionary representing the parsed configuration.
     """
     config_dict = {}
     current_section = None
+    skip_lines = False
 
     with open(filename, 'r') as file:
         for line in file:
             line = line.strip()
-            if not line or line.startswith("#"):
+            if skip_header and line.startswith("<comment>"):
+                skip_lines = True
+                continue
+            if skip_lines and line == "":
+                skip_lines = False
+
+            if not line or line.startswith("#") or (skip_header and skip_lines):
                 continue
             elif line.startswith("<") and line.endswith(">"):
                 current_section = line[1:-1]
                 config_dict[current_section] = []
             elif current_section is not None:
-                parts = line.split('#', 1)  # Split at the first '#' to separate key and comment
+                parts = line.split('#', 1)
                 if len(parts) == 2:
                     key_value_part, comment = map(str.strip, parts)
                 else:
@@ -31,16 +110,40 @@ def read_athenapk_config_file(filename: str) -> dict:
     return config_dict
 
 
+def modify_athenapf_config_file(config: dict,
+                                modifications: list) -> dict:
+    """
+    Modify a configuration dictionary based on a list of specified modifications.
+
+    Args:
+        config (dict): The original configuration dictionary to be modified.
+        modifications (list): A list of modifications, where each modification is a tuple
+        (section, key, new_value) specifying the section, key, and the new value to be set.
+
+    Returns:
+        dict: The modified configuration dictionary.
+    """
+    modified_config = config.copy()
+    for section, key, new_value in modifications:
+        if section in modified_config:
+            for i, (config_key, value, comment) in enumerate(modified_config[section]):
+                if config_key == key:
+                    modified_config[section][i] = (key, new_value, comment)
+    return modified_config
+                
+
 def write_athenapk_config_file(filename: str,
                                config_dict: dict,
-                               min_column_width: int = 10) -> None:
+                               number_of_cells: int = 512,
+                               column_widths: dict = None) -> None:
     """
-    Write an AthenaPK configuration to a file with optional column padding.
+    Write an AthenaPK configuration to a file with custom column padding widths and substitute `number_of_cells`.
 
     Parameters:
         filename (str): The name of the output configuration file.
         config_dict (dict): The configuration dictionary to write.
-        min_column_width (int, optional): Minimum width for each column. Default is 10.
+        number_of_cells (int, optional): The number of cells to substitute in the configuration.
+        column_widths (dict, optional): Dictionary specifying custom column widths for sections.
 
     Returns:
         None
@@ -48,18 +151,41 @@ def write_athenapk_config_file(filename: str,
     with open(filename, 'w') as file:
         for section, options in config_dict.items():
             file.write(f"<{section}>\n")
-            for key, value, comment in options:
-                value = str(value)
-                key_padding = max(0, min_column_width - len(key))
-                value_padding = max(0, min_column_width - len(value) if comment else 0)
-                comment_padding = max(0, min_column_width - len(comment) if comment else 0)
 
-                formatted_line = f"{key}{' ' * key_padding} = {value}{' ' * value_padding}"
-                if comment:
-                    formatted_line += f"{' ' * comment_padding} # {comment}"
+            if column_widths and section in column_widths:
+                key_width, value_width, comment_width = column_widths[section]
+            else:
+                key_width, value_width, comment_width = 10, 10, 10
+
+            for key, value, comment in options:
+                key = str(key)
+                value = str(value)
+                comment = str(comment) if comment is not None else ""
+
+                if section == "parthenon/mesh" and key.startswith("nx"):
+                    value = str(number_of_cells)
+                elif section == "parthenon/meshblock" and key.startswith("nx"):
+                    if key == "nx1" or key == "nx2":
+                        value = str(number_of_cells // 4)
+                    elif key == "nx3":
+                        value = str(number_of_cells // 2)
+
+                key_padding = max(0, key_width - len(key))
+                value_padding = max(0, value_width - len(value) if comment else 0)
+                comment_padding = max(0, comment_width - len(comment) if comment else 0)
+
+                if section == "parthenon/mesh":
+                    if key == "nx2" or key == "nx3":
+                        formatted_line = "\n"
+                    else:
+                        formatted_line = ""
+                    formatted_line += f"{key}{' ' * key_padding} = {value}{' ' * value_padding}{' ' * comment_padding}{' # ' + comment if comment else ''}"
+                else:
+                    formatted_line  = f"{key}{' ' * key_padding} = {value}{' ' * value_padding}{' ' * comment_padding}{' # ' + comment if comment else ''}"
                 formatted_line += "\n"
 
                 file.write(formatted_line)
+
             file.write("\n")
 
 
