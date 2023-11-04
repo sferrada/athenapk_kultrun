a38fb35548c29bdf8a19efec1aac4c0a50d8638d
Simon <simonfch72@gmail.com>
2023-11-03 21:35:33 -0300

    Backup

--- submit_run.sh
+++ submit_run.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
+#SBATCH --job-name=turb_athenapk
+#SBATCH --partition=kurruf_gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=40G
+#SBATCH --gres=gpu:A100:1
+
+# Load modules, etc.
+module load openmpi/4.1.5 gcc/12.2.0 hdf5/1.14.1-2_openmpi-4.1.5_parallel cuda/12.2
 
 # Parse config file
 source scripts/parse_yaml_file.sh
 eval $(parse_yaml config.yaml)
 EOS="${equation_of_state:0:5}"
 EOS="${EOS^}"
-
-# Set Slurm parameters from the config file
-JOB_NAME="${job_name}"
-NODES="${number_of_nodes}"
-NTASKS="${number_of_tasks}"
-NGPU="${number_of_gpus}"
-
-# Slurm directives
-#SBATCH --job-name="$JOB_NAME"
-#SBATCH --partition=kurruf_gpu
-#SBATCH --nodes="$NODES"
-#SBATCH --ntasks-per-node="$NTASKS"
-#SBATCH --mem=40G
-#SBATCH --gres=gpu:A100:"$NGPU"
-
-# Load modules, etc.
-module load openmpi/4.1.5 gcc/12.2.0 hdf5/1.14.1-2_openmpi-4.1.5_parallel cuda/12.2
 
 # Set directory names
 PRJDIR=/home/sferrada/athenapk_kultrun
@@ -31,7 +23,8 @@ RUNDIR=Turb_nGPUs1_ncells${number_of_cells}_accelrms${acceleration_field_rms}_B$
 cd $PRJDIR
 mpirun ./athenapk/build-host/bin/athenaPK -i ./outputs/${RUNDIR}/turbulence_philipp.in -d ./outputs/${RUNDIR}/ > "./outputs/${RUNDIR}/turbulence_philipp.out"
 
-# Run post-analysis if specified in the config file, e.g., ("mach_number" "density")
+# Run post-analysis if specified in the config file for a list of fields,
+# e.g., fields_for_analysis=("mach_number" "density")
 if [[ $run_analysis = "True" ]]; then
     fields_for_analysis=("mach_number")
     if [[ ${#fields_for_analysis[@]} -eq 0 ]]; then
@@ -46,5 +39,5 @@ else
     echo "Post-run analysis is not enabled in the config."
 fi
 
-# After its done, move all output files to the correspondant folder
-mv parthenon.* slurm-*.out ./outputs/${RUNDIR}/
+
+
