765f35b1356120d2a37c471965583366bfe5fe32
Simon <simonfch72@gmail.com>
2024-04-06 12:03:29 -0300

    Rerun 3 runs

--- submit_run.sh
+++ submit_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=athenapk_gpu
 #SBATCH --partition=kurruf_gpu
-#SBATCH --gres=gpu:A100:1
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem="90G"
+#SBATCH --ntasks-per-node=8
+#SBATCH --gres=gpu:A100:1
+#SBATCH --mem="100G"
+#SBATCH --exclusive
 #SBATCH --verbose
 
 # Load modules
@@ -12,8 +13,11 @@ module load openmpi/4.1.5 gcc/12.2.0 hdf5/1.14.1-2_openmpi-4.1.5_parallel cuda/1
 
 # Set directory names
 MAIN_DIR=/home/sferrada/athenapk_kultrun
-SIM_DIR=NG_1-NC_128-TCOR_1.00-SOLW_1.00-ARMS_1.00-BINI_0.05-EOSG_1.00
+SIM_DIR=NG_1-NC_256-TCOR_1.00-SOLW_1.00-ARMS_1.00-BINI_1.00-EOSG_1.00
 OUT_DIR=outputs/${SIM_DIR}
+
+# Choose CUDA device
+export CUDA_VISIBLE_DEVICES=2
 
 # Run the sim
 cd $MAIN_DIR
